8bedeb5d6b7deeaced0f12dbb3db00532fbb1e15
Carlos O'Ryan <coryan@google.com>
2026-07-27 18:20:35 -0400

    chore: update librarian circa 2026-07-27 (#48)
    In this version, librarian generates correct cross-reference links for
    enums. This fixes a number of documentation warnings for
    `google-cloud-wkt` and many generated libraries.

--- Sources/GoogleRpc/Code.swift
+++ Sources/GoogleRpc/Code.swift
@@ -234,7 +234,7 @@ public enum Code: Codable, Equatable, Sendable {
 
   /// Initialize from a string value.
   ///
-  /// If the value is unknown, this initializes to ``.unknownStringValue(_:)``.
+  /// If the value is unknown, this initializes to [`unknownStringValue`](doc:Code/unknownStringValue(_:)).
   public init(stringValue: Swift.String) {
     switch stringValue {
     case "OK": self = .ok
@@ -260,7 +260,7 @@ public enum Code: Codable, Equatable, Sendable {
 
   /// Initialize from an integer value.
   ///
-  /// If the value is unknown, this initializes to ``.unknownIntValue(_:)``.
+  /// If the value is unknown, this initializes to [`unknownIntValue`](doc:Code/unknownIntValue(_:)).
   public init(intValue: Int) {
     switch intValue {
     case 0: self = .ok
